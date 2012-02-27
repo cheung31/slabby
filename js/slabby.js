2eb0a55f60f58f41c2102cdb92c03cd4c58b9631
Ryan Cheung <cheung31@gmail.com>
2012-02-26 20:57:25 -0800

    Add knob, set width based on number of slabs in slabby

--- js/slabby.js
+++ js/slabby.js
@@ -4,13 +4,19 @@ $(document).ready(function(){
 
 var slabby = {
     pageSetup: function(){
+        slabby.$slab = $('.slabby:visible');
+
         slabby.setupSlider();
     },
 
     setupSlider: function(){
         var $slider;
 
+        // center slider
         $slider = $('#slider');
         $slider.css('margin-left', (-1*($slider.width()/2))+'px');
+
+        // set knob width based on num slabs
+        $('#knob', $slider).css('width', slabby.$slab.width() / $('.slab').length);
     }
 };
