985a900b3b6ee92934190ee9d7613cfc2d70eb8d
Ryan Cheung <cheung31@gmail.com>
2012-03-04 21:08:59 -0800

    Knob width proportional to a slab width and total number of slabs.

--- js/slabby.js
+++ js/slabby.js
@@ -7,6 +7,7 @@ var slabby = {
         slabby.$slab = $('.slabby:visible');
 
         slabby.setupSlider();
+        slabby.setupKnob();
     },
 
     setupSlider: function(){
@@ -15,8 +16,24 @@ var slabby = {
         // center slider
         $slider = $('#slider');
         $slider.css('margin-left', (-1*($slider.width()/2))+'px');
+        slabby.$slider = $slider;
+    },
+
+    setupKnob: function(){
+        var $knob,
+            knob_width,
+            $slabs,
+            slabs_width;
 
         // set knob width based on num slabs
-        $('#knob', $slider).css('width', slabby.$slab.width() / $('.slab').length);
+        $knob = $('#knob');
+
+        $slabs = $('li.slab');
+        slabs_width = $slabs.length * $slabs.eq(0).width();
+        console.log(slabs_width);
+        
+        knob_width = (slabby.$slider.width() * $slabs.eq(0).width()) / slabs_width;
+        $knob.css('width', knob_width);
+        slabby.$knob = $knob;
     }
 };
