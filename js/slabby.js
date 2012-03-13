a69a095dd1fb9325aeafbfe577ce650ac921e6ee
Ryan Cheung <cheung31@gmail.com>
2012-03-13 15:19:56 -0700

    Draggable slider knob

--- js/slabby.js
+++ js/slabby.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 var slabby = {
     pageSetup: function(){
         slabby.$slab = $('.slabby:visible');
+        slabby.$slider = null;
+        slabby.$knob = null;
 
         slabby.setupSlider();
         slabby.setupKnob();
@@ -12,8 +14,8 @@ var slabby = {
         slabby.setupMouse();
 
         $(window).resize(function(){
+            slabby.$slider = null;
             slabby.setupSlider();
-            slabby.setupKnob();
         });
     },
 
@@ -42,6 +44,8 @@ var slabby = {
         
         knob_width = (slabby.$slider.width() * $slabs.eq(0).width()) / slabs_width;
         $knob.css('width', knob_width);
+        $knob._dragging = false;
+
         slabby.$knob = $knob;
     },
 
@@ -52,6 +56,35 @@ var slabby = {
 
 
     setupMouse: function(){
+        // setup draggable knob
+        slabby.$knob.mousedown(function(e){
+            //console.log('mousedown');
+            slabby.$knob._dragging = true;
+            slabby.$knob._mouseOffset = [e.pageX, e.pageY];
+        });
+        $(window).mousemove(function(e){
+            var d,
+                knob_left,
+                knob_min_left,
+                knob_max_left;
+            if (!slabby.$knob._dragging)
+                return;
 
+            knob_min_left = slabby.$slider.offset().left;
+            knob_max_left = slabby.$slider.width() - slabby.$knob.width();
+
+            d = e.pageX - slabby.$knob._mouseOffset[0];
+            knob_left = parseInt(slabby.$knob.css('left')) || 0;
+            if (knob_left >= 0 && knob_left <= knob_max_left) {
+                if (knob_left + d > 0 && knob_left <= knob_max_left)
+                    slabby.$knob.css('left', d);
+            } else {
+                return;
+            }
+        });
+        $(window).mouseup(function(e){
+            //console.log('mouseup');
+            slabby.$knob._dragging = false;
+        });
     }
 };
