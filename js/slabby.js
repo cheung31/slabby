23c2bd39ce70c724aaa89dd73c2f4a3aac33a669
Ryan Cheung <cheung31@gmail.com>
2012-04-17 17:30:27 -0700

    remove stackblurimage js

--- js/slabby.js
+++ js/slabby.js
@@ -312,28 +312,3 @@ Slabby.prototype = {
     }
 };
 
-function stackBlurImage( imageID, canvasID, radius, blurAlphaChannel )
-{
-    var img = document.getElementById( imageID );
-    var w = img.naturalWidth;
-    var h = img.naturalHeight;
-
-    var canvas = document.getElementById( canvasID );
-
-    canvas.style.width  = w + "px";
-    canvas.style.height = h + "px";
-    canvas.width = w;
-    canvas.height = h;
-
-    var context = canvas.getContext("2d");
-    context.fillStyle = "rgba(255,255,255,.75)";
-    context.fillRect( 0, 0, w, h );
-    context.drawImage( img, 5, 5, w/3, h/3 );
-
-    if ( isNaN(radius) || radius < 1 ) return;
-
-    if ( blurAlphaChannel )
-        stackBlurCanvasRGBA( canvasID, 0, 0, w, h, radius );
-    else 
-        stackBlurCanvasRGB( canvasID, 0, 0, w, h, radius );
-}
