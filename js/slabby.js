953e349f042de77740afa4cb3880a0c79fbec71a
Ryan Cheung <cheung31@gmail.com>
2013-03-10 18:34:12 -0700

    Add throttling to paging

--- js/slabby.js
+++ js/slabby.js
@@ -1,42 +1,3 @@
-/*
- * Normalized hide address bar for iOS & Android
- * (c) Scott Jehl, scottjehl.com
- * MIT License
- */
-(function( win ){
- var doc = win.document;
-
- // If there's a hash, or addEventListener is undefined, stop here
- if( !location.hash && win.addEventListener ){
-
- //scroll to 1
- window.scrollTo( 0, 1 );
- var scrollTop = 1,
- getScrollTop = function(){
- return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
- },
-
- //reset to 0 on bodyready, if needed
- bodycheck = setInterval(function(){
-     if( doc.body ){
-     clearInterval( bodycheck );
-     scrollTop = getScrollTop();
-     win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-     }   
-     }, 15 );
-
-win.addEventListener( "load", function(){
-        setTimeout(function(){
-            //at load, if user hasn't scrolled more than 20 or so...
-            if( getScrollTop() < 20 ){
-            //reset to hide addr bar at onload
-            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-            }
-            }, 0);
-        } );
- }
-})( this );
-
 var _s = {
     Router : Backbone.Router.extend({
         routes: {
@@ -258,7 +219,7 @@ Slabby.prototype = {
 
     setupKeyboard: function () {
         var _slabby = this;
-        $(document).keydown(function (e) {
+        $(document).keydown($.throttle(200, function (e) {
             if(!_slabby.isActive())
                 return;
 
@@ -278,7 +239,7 @@ Slabby.prototype = {
             } else {
                 _slabby._jumping = false;
             }
-        });
+        }));
     },
 
     setupMouse: function () {
