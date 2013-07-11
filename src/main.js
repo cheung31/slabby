9ffd7cf15787976ddf9e35d9f1297062e892b1d3
Ryan Cheung <cheung31@gmail.com>
2013-07-11 02:55:34 -0700

    Create main. Create stream model. Create stream types. Create slab model.

---
+++ src/main.js
@@ -0,0 +1,21 @@
+define(['slabby', 'slabby/streams/instagram', 'slabby/streams/projects', 'slabby/streams/lastfm'],
+function(Slabby, InstagramStream, ProjectsStream, LastFmStream) {
+    var containerEl = document.getElementById('#container');
+    var slabby = new Slabby(containerEl);
+    slabby.addStream(
+        new InstagramStream('photos', {
+            access_token: '13865411.8167e48.b43836f60d6d4d53a28b996418e31d17'
+            user_id: '13865411'
+        })
+    );
+    slabby.addStream(
+        new ProjectsStream('projects')
+    );
+    slabby.addStream(
+        new LastFmStream('tunes', {
+            api_key: '9bea97674bc0b61b9d6807055313155e'
+            user: 'cheung31'
+        })
+    );
+    slabby.start();
+});
