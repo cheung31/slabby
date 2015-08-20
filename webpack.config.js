7f8dc93360f4a4c2c8a98123ce74c06450fca147
Ryan Cheung <cheung31@gmail.com>
2015-08-19 22:23:11 -0400

    Move jquery dep to package.json'

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         root: path.join(__dirname, 'src'),
         modulesDirectories: ['public/js', 'node_modules'],
         alias: {
-            'jquery': path.join(__dirname, 'lib/jquery/dist/jquery'),
+            'jquery': path.join(__dirname, 'node_modules/jquery/dist/jquery'),
             'text': path.join(__dirname, 'lib/requirejs-text/text'),
             'hgn': path.join(__dirname, 'lib/requirejs-hogan-plugin/hogan'),
             'eventEmitter': path.join(__dirname, 'lib/eventEmitter/EventEmitter'),
