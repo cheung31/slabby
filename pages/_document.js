dea5181de8be823203688252d0db4c44698cd85f
Ryan Cheung <cheung31@gmail.com>
2021-04-08 23:04:22 -0700

    CustomDocument

--- pages/_document.js
+++ pages/_document.js
@@ -1,2 +1,25 @@
 // @generated: @expo/next-adapter@2.1.5
-export { default } from '@expo/next-adapter/document'
+import { getInitialProps } from '@expo/next-adapter/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class CustomDocument extends Document {
+    render() {
+        return (
+            <Html>
+            <Head>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+            </Html>
+        );
+    }
+}
+
+// Import the getInitialProps method and assign it to your component to ensure the react-native-web styles are used.
+CustomDocument.getInitialProps = getInitialProps;
+
+export default CustomDocument;
