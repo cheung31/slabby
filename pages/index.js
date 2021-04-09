eb9dd25344e11ee82dafd589c451a49076300ee6
Ryan Cheung <cheung31@gmail.com>
2021-04-09 00:41:03 -0700

    Custom font

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,40 @@
 // @generated: @expo/next-adapter@2.1.5
-import { StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Layout, Text, Spinner } from '@ui-kitten/components';
 import { default as theme } from './theme.json';
+import { default as mapping } from './mapping.json';
+import OpenSansRegular from './OpenSans-Regular.ttf';
 
 export default function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await Font.loadAsync({
+          OpenSansRegular: {
+            uri: OpenSansRegular,
+            fontDisplay: Font.FontDisplay.SWAP
+          }
+        });
+      } catch ({ message }) {
+        // This will be called if something is broken
+        console.log(`Error loading font: ${message}`);
+      } finally {
+        setLoaded(true);
+      }
+    })();
+  }, []);
+
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} customMapping={mapping}>
       <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text category='h1'>Welcome to Expo + Next.js 👋</Text>
+        {!loaded ? <Spinner size='giant'/> :
+          <Text category='h1'>Welcome to Expo + Next.js 👋</Text>
+        }
       </Layout>
     </ApplicationProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-  },
-})
