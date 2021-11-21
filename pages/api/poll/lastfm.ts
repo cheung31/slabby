88156af4b5115f5323d47ceb16fe6a054a2bedbe
Ryan Cheung <cheung31@gmail.com>
2021-11-20 22:53:35 -0800

    poll - lastfm

---
+++ pages/api/poll/lastfm.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PostgrestError } from "@supabase/supabase-js";
+import LastFm from '@toplast/lastfm'
+import { supabase } from '../../../utils/supabaseClient'
+import { definitions } from "../../../types/supabase";
+
+type Data = definitions['things'][] | null | PostgrestError
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method !== 'POST') {
+        return res.status(400)
+    }
+
+    if (!process.env.LASTFM_API_KEY) {
+        return res.status(500)
+    }
+
+    const lastFm = new LastFm(process.env.LASTFM_API_KEY)
+    const user = req.query.user ?
+        Array.isArray(req.query.user) ? req.query.user[0] : req.query.user
+        : 'cheung31'
+
+    const recentTracks = await lastFm.user.getRecentTracks({ user })
+    const records = recentTracks.recenttracks.track
+        .map<definitions['things']>((track) => {
+            return {
+                id: "",
+                type: "tune",
+                external_id: `${track.mbid}::${track.date?.uts}`,
+                external_url: track.url,
+                title: track.name,
+                description: track.artist.name,
+                content_date: track.date?.uts,
+                created_at: "",
+                updated_at: ""
+            }
+        })
+
+    const { data, error } = await supabase
+        .from<definitions['things']>('things')
+        .upsert(records, { ignoreDuplicates: true })
+
+    if (error) {
+        return res.status(500).json(error)
+    }
+
+    res.status(200).json(data)
+}
