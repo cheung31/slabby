e0aeab029b101cd78a6092acb1901859225dbbdd
Ryan Cheung <cheung31@gmail.com>
2021-11-21 16:06:59 -0800

    Paginate through all fsq photos

--- pages/api/poll/foursquare.ts
+++ pages/api/poll/foursquare.ts
@@ -14,7 +14,9 @@ type Error = {
 type Data =  definitions['things'][] | IPayload<any> | null | PostgrestError | PostgrestError[] | Error
 
 type PostQuery = {
-    user_id?: string
+    user_id?: string,
+    limit?: string,
+    offset?: string
 }
 
 interface IPhoto extends NPhotos.IPhoto {
@@ -40,82 +42,93 @@ async function post(
         return res.status(500).json({ error: 'Missing Foursquare Credentials' })
     }
 
+    let hasMorePages = true
     const query = req.query as PostQuery
-    const user_id = query.user_id || process.env.FOURSQUARE_USER_ID
-    const response = await fetch(
-        `https://api.foursquare.com/v2/users/${user_id}/photos?v=20210101&client_id=${process.env.FOURSQUARE_CLIENT_ID}&client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}&oauth_token=${process.env.FOURSQUARE_ACCESS_TOKEN}`
-    )
-    const api_response = await response.json() as IPayload<IPhotosResponse>
-    if (!api_response) {
-        return res.status(500).json({ error: 'Foursquare API request failed' })
-    }
-
-    const response_status = api_response.meta?.code || 500
-    if (response_status !== 200) {
-        return res.status(response_status).json(api_response)
-    }
-
-    const photos = api_response.response?.photos
-    if (!photos) {
-        return res.status(200).json({})
-    }
-
-    const records = photos.items.map((photo) => {
-        let timestampz
-        if (photo.createdAt) {
-            timestampz = utcStringToTimestampz(photo.createdAt.toString())
-        }
-
-        let title
-        let external_url
-        if (photo.tip) {
-            title = photo.tip.text
-            external_url = photo.tip.canonicalUrl
-        }
-
-        let description
-        if (photo.venue) {
-            const { city, state, country } = photo.venue.location
-            const location = city && state
-                ? `${city}, ${state}`
-                : city && country
-                    ? `${city}, ${country}`
-                    : city
-            description = `${photo.venue.name} - ${location}`
-        }
-
-        const size = '610x610'
-        const image_url = `${photo.prefix}${size}${photo.suffix}`
-
-       return {
-           type: "photo",
-           external_source: "Foursquare",
-           external_id: photo.id,
-           external_url,
-           title,
-           description,
-           image_url,
-           content_date: timestampz, // "2021-11-21T07:13:48.000Z"
-       }
-    })
-
-    const groupedRecords = groupUpserts(records)
+    const limit = query.limit ? parseInt(query.limit) : 200
+    let offset = query.offset ? parseInt(query.offset) : 0
     const errors = []
     let processed: definitions['things'][] = []
+    while(hasMorePages) {
+        const user_id = query.user_id || process.env.FOURSQUARE_USER_ID
+        let url = `https://api.foursquare.com/v2/users/${user_id}/photos?v=20210101&client_id=${process.env.FOURSQUARE_CLIENT_ID}&client_secret=${process.env.FOURSQUARE_CLIENT_SECRET}&oauth_token=${process.env.FOURSQUARE_ACCESS_TOKEN}`
+        url = `${url}&offset=${offset}&limit=${limit}`
 
-    for (const k in groupedRecords) {
-        const rs = groupedRecords[k]
-
-        const { data, error } = await supabase
-            .from<definitions['things']>('things')
-            .upsert(rs, { onConflict: 'external_source,external_id', ignoreDuplicates: true })
-
-        if (error) {
-            errors.push(error)
+        const response = await fetch(url)
+        const api_response = await response.json() as IPayload<IPhotosResponse>
+        if (!api_response) {
+            return res.status(500).json({ error: 'Foursquare API request failed' })
         }
 
-        if (data) {
-            processed = processed.concat(data)
+        const response_status = api_response.meta?.code || 500
+        if (response_status !== 200) {
+            return res.status(response_status).json(api_response)
+        }
+
+        const photos = api_response.response?.photos
+        if (!photos) {
+            return res.status(200).json({})
+        }
+
+        const records = photos.items.map((photo) => {
+            let timestampz
+            if (photo.createdAt) {
+                timestampz = utcStringToTimestampz(photo.createdAt.toString())
+            }
+
+            let title
+            let external_url
+            if (photo.tip) {
+                title = photo.tip.text
+                external_url = photo.tip.canonicalUrl
+            }
+
+            let description
+            if (photo.venue) {
+                const { city, state, country } = photo.venue.location
+                const location = city && state
+                    ? `${city}, ${state}`
+                    : city && country
+                        ? `${city}, ${country}`
+                        : city
+                description = `${photo.venue.name} - ${location}`
+            }
+
+            const size = '610x610'
+            const image_url = `${photo.prefix}${size}${photo.suffix}`
+
+            return {
+                type: "photo",
+                external_source: "Foursquare",
+                external_id: photo.id,
+                external_url,
+                title,
+                description,
+                image_url,
+                content_date: timestampz, // "2021-11-21T07:13:48.000Z"
+            }
+        })
+
+        const groupedRecords = groupUpserts(records)
+
+        for (const k in groupedRecords) {
+            const rs = groupedRecords[k]
+
+            const { data, error } = await supabase
+                .from<definitions['things']>('things')
+                .upsert(rs, { onConflict: 'external_source,external_id', ignoreDuplicates: true })
+
+            if (error) {
+                errors.push(error)
+            }
+
+            if (data) {
+                processed = processed.concat(data)
+            }
+        }
+
+        hasMorePages = offset + photos.items.length < photos.count
+        if (hasMorePages) {
+            offset = offset + photos.items.length
         }
     }
 
