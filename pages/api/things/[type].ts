e8e93f5221a7fb41b4f5de8ad10c099622adf8cd
Ryan Cheung <cheung31@gmail.com>
2021-11-21 17:31:22 -0800

    Configure publish delay offset per type

--- pages/api/things/[type].ts
+++ pages/api/things/[type].ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { PostgrestError } from "@supabase/supabase-js";
 import { supabase } from '../../../utils/supabaseClient'
 import { definitions, paths } from "../../../types/supabase";
+import { utcStringToTimestampz } from "../../../utils";
 
 type Error = {
     error: string
@@ -13,7 +14,19 @@ type GetPathQuery = {
 }
 type GetQuery = paths["/things"]["get"]["parameters"]["query"]
 
+const thingTypes = ['tune', 'photo'] as const;
+type ThingType = typeof thingTypes[number]
+
 const DEFAULT_PAGE_SIZE = 10
+const TYPE_PUBLISH_DELAY_MS: Record<ThingType, number> = {
+    'tune': 0,
+    'photo': 7 * 24 * 3600 * 1000
+}
+
+function isThingType(type: unknown): type is ThingType {
+    const foundKey = thingTypes.find((t) => t === type);
+    return !!foundKey
+}
 
 async function get(
     req: NextApiRequest,
@@ -21,18 +34,22 @@ async function get(
 ) {
     const query = req.query as GetQuery
     const type = query.type
-    if (!type) {
+    if (!type || !isThingType(type)) {
         return res.status(400).json({ error: 'Missing type' })
     }
+
     const limit = parseInt(query.limit || DEFAULT_PAGE_SIZE.toString())
     const rangeFrom = parseInt(query.offset || "0")
     const rangeTo = rangeFrom + limit - 1
 
-    const { data, error } = await supabase
+    const contentDateOffset = `${utcStringToTimestampz(((Date.now() - TYPE_PUBLISH_DELAY_MS[type])/1000).toString())}`
+
+    const {data, error} = await supabase
         .from<definitions['things']>('things')
         .select(query.select)
         .eq('type', type)
-        .order('content_date', { ascending: false })
+        .lte('content_date', contentDateOffset)
+        .order('content_date', {ascending: false})
         .limit(limit)
         .range(rangeFrom, rangeTo)
 
