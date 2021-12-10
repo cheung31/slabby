6918d6a516a2486223ca78c80d8694957989016a
Ryan Cheung <cheung31@gmail.com>
2021-12-10 00:06:01 -0800

    MobileNav hover states

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -12,7 +12,7 @@ const MobileNav = ({ pathname = "", className  = ""}: MobileNavProps) => {
     return (
         <div className={`flex font-mono shadow-lg dark:text-gray-300 w-screen ${className}`}>
             <BlurOverlay
-                className={`flex-1 p-3 pt-2 ${pathname === 'photo' ? styles.active : styles.tab}`}
+                className={`flex-1 p-3 pt-2 hover:bg-opacity-40 dark:hover:bg-opacity-70 ${pathname === 'photo' ? styles.active : styles.tab}`}
             >
                 <Link href="/photo">
                     <div className="cursor-pointer">photos</div>
@@ -20,7 +20,7 @@ const MobileNav = ({ pathname = "", className  = ""}: MobileNavProps) => {
             </BlurOverlay>
             <Link href="/tune">
                 <BlurOverlay
-                    className={`flex-1 p-3 pt-2 ${pathname === 'tune' ? styles.active : styles.tab}`}>
+                    className={`flex-1 p-3 pt-2 hover:bg-opacity-40 dark:hover:bg-opacity-70 ${pathname === 'tune' ? styles.active : styles.tab}`}>
                     <Link href="/tune">
                         <div className="cursor-pointer">tunes</div>
                     </Link>
