93440bb9d2e23bf12823b66959d2ce6520e7c292
Ryan Cheung <cheung31@gmail.com>
2021-12-22 15:57:43 -0700

    prettier, prettier-eslint, husky, lint-staged

--- pages/[type].tsx
+++ pages/[type].tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
 import Plx from 'react-plx'
 import type { NextPage } from 'next'
-import {useRouter} from "next/router"
-import MobileNav from "../components/MobileNav"
-import RandomQuote from "../components/RandomQuote"
-import GenericTimeline from "../components/GenericTimeline"
-import {isThingType} from "../types/things"
+import { useRouter } from 'next/router'
+import MobileNav from '../components/MobileNav'
+import RandomQuote from '../components/RandomQuote'
+import GenericTimeline from '../components/GenericTimeline'
+import { isThingType } from '../types/things'
 
 const Index: NextPage = () => {
     const router = useRouter()
@@ -18,64 +17,73 @@ const Index: NextPage = () => {
 
     return (
         <>
-            <MobileNav className="z-50 fixed bottom-0 px-3 pb-3" pathname={type} />
+            <MobileNav
+                className="z-50 fixed bottom-0 px-3 pb-3"
+                pathname={type}
+            />
             <div className="container mx-auto">
                 <div className="pt-2 xs:pt-8" style={{ paddingBottom: '47vh' }}>
                     {timeline}
                     <>
-                        <Plx parallaxData={[
-                            {
-                                start: 'self',
-                                duration: 700,
-                                easing: 'easeOut',
-                                properties: [
-                                    {
-                                        startValue: 200,
-                                        endValue: 0,
-                                        property: 'translateY',
-                                    },
-                                    {
-                                        startValue: 0,
-                                        endValue: 1,
-                                        property: 'scale',
-                                    },
-                                    {
-                                        startValue: 0,
-                                        endValue: .6,
-                                        property: 'opacity',
-                                    },
-                                ],
-                            },
-                        ]}>
-                            <div className="mx-auto mt-5 bg-gradient-to-b from-gray-800 to-gray-300 dark:from-gray-300 dark:to-gray-800"
-                                 style={{width: 2, height: "75vh"}}/>
+                        <Plx
+                            parallaxData={[
+                                {
+                                    start: 'self',
+                                    duration: 700,
+                                    easing: 'easeOut',
+                                    properties: [
+                                        {
+                                            startValue: 200,
+                                            endValue: 0,
+                                            property: 'translateY',
+                                        },
+                                        {
+                                            startValue: 0,
+                                            endValue: 1,
+                                            property: 'scale',
+                                        },
+                                        {
+                                            startValue: 0,
+                                            endValue: 0.6,
+                                            property: 'opacity',
+                                        },
+                                    ],
+                                },
+                            ]}
+                        >
+                            <div
+                                className="mx-auto mt-5 bg-gradient-to-b from-gray-800 to-gray-300 dark:from-gray-300 dark:to-gray-800"
+                                style={{ width: 2, height: '75vh' }}
+                            />
                         </Plx>
-                        <Plx parallaxData={[
-                            {
-                                start: 'self',
-                                duration: 150,
-                                easing: 'easeOut',
-                                properties: [
-                                    {
-                                        startValue: 400,
-                                        endValue: 0,
-                                        property: 'translateY',
-                                    },
-                                ],
-                            },
-                            {
-                                start: 'self',
-                                duration: 700,
-                                easing: 'easeOut',
-                                properties: [
-                                    {
-                                        startValue: 0,
-                                        endValue: 1,
-                                        property: 'opacity',
-                                    },
-                                ],
-                            },
-                        ]}>
+                        <Plx
+                            parallaxData={[
+                                {
+                                    start: 'self',
+                                    duration: 150,
+                                    easing: 'easeOut',
+                                    properties: [
+                                        {
+                                            startValue: 400,
+                                            endValue: 0,
+                                            property: 'translateY',
+                                        },
+                                    ],
+                                },
+                                {
+                                    start: 'self',
+                                    duration: 700,
+                                    easing: 'easeOut',
+                                    properties: [
+                                        {
+                                            startValue: 0,
+                                            endValue: 1,
+                                            property: 'opacity',
+                                        },
+                                    ],
+                                },
+                            ]}
+                        >
                             <RandomQuote uniq={type} />
                         </Plx>
                     </>
