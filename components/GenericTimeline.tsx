3d100792378a03008be5901d60d083c629e99018
Ryan Cheung <cheung31@gmail.com>
2022-04-30 18:39:51 -0700

    Move options to config

--- components/GenericTimeline.tsx
+++ components/GenericTimeline.tsx
@@ -1,18 +1,8 @@
 import { useCallback } from 'react'
 import { Timeline } from './Timeline'
-import { useThings, useThingsOptions } from '../hooks/useThings'
+import { useThings } from '../hooks/useThings'
 import { ThingType } from '../types/things'
-
-const typeOptions: Record<ThingType, useThingsOptions> = {
-    tune: {
-        limit: 25,
-        pollIntervalMs: 30 * 1000,
-    },
-    photo: {
-        limit: 25,
-        pollIntervalMs: 2 * 60 * 1000,
-    },
-}
+import { typeOptions } from '../config'
 
 const GenericTimeline = ({ type }: { type: ThingType }) => {
     const {
--- config.ts
+++ config.ts
@@ -1,7 +1,19 @@
 import { ThingType } from './types/things'
+import { useThingsOptions } from './hooks/useThings'
 
 export const DEFAULT_PAGE_SIZE = 10
 export const TYPE_PUBLISH_DELAY_MS: Record<ThingType, number> = {
-    'tune': 0,
-    'photo': 7 * 24 * 3600 * 1000
+    tune: 0,
+    photo: 7 * 24 * 3600 * 1000,
+}
+
+export const typeOptions: Record<ThingType, useThingsOptions> = {
+    tune: {
+        limit: 25,
+        pollIntervalMs: 30 * 1000,
+    },
+    photo: {
+        limit: 25,
+        pollIntervalMs: 2 * 60 * 1000,
+    },
 }
