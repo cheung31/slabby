78a4b2d4292f3c4e4ded4d6892745659ae214602
Ryan Cheung <cheung31@gmail.com>
2022-05-14 10:56:27 -0700

    Memoize thingType in generic page. Wait til timeline to render random quote.

--- pages/[type].tsx
+++ pages/[type].tsx
@@ -1,6 +1,7 @@
-import Plx from 'react-plx'
+import { useMemo } from 'react'
 import type { GetServerSideProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
+import Plx from 'react-plx'
 import MobileNav from '../components/MobileNav'
 import RandomQuote from '../components/RandomQuote'
 import GenericTimeline from '../components/GenericTimeline'
@@ -12,24 +13,26 @@ import { utcStringToTimestampz } from '../utils'
 import { TYPE_PUBLISH_DELAY_MS } from '../config'
 
 type IndexProps = {
-    type?: ThingType
+    thingType?: ThingType
     timelineItems?: TimelineItem[]
 }
-const Index: NextPage<IndexProps> = ({ type: t, timelineItems }) => {
+const Index: NextPage<IndexProps> = ({ thingType: t, timelineItems }) => {
     const router = useRouter()
-    const { type: queryType } = router.query
-    const type = t || queryType
 
-    let timeline
-    if (isThingType(type)) {
-        timeline = <GenericTimeline type={type} initialItems={timelineItems} />
-    }
+    const thingType = useMemo(() => {
+        const { type: queryType } = router.query
+        return t || queryType
+    }, [t, router])
+
+    const timeline = isThingType(thingType) ? (
+        <GenericTimeline type={thingType} initialItems={timelineItems} />
+    ) : null
 
     return (
         <>
             <MobileNav
                 className="z-50 fixed bottom-0 px-3 pb-3"
-                pathname={type}
+                pathname={thingType}
             />
             <div className="container mx-auto">
                 <div className="pt-2 xs:pt-8" style={{ paddingBottom: '47vh' }}>
@@ -66,36 +69,38 @@ const Index: NextPage<IndexProps> = ({ type: t, timelineItems }) => {
                                 style={{ width: 2, height: '75vh' }}
                             />
                         </Plx>
-                        <Plx
-                            parallaxData={[
-                                {
-                                    start: 'self',
-                                    duration: 150,
-                                    easing: 'easeOut',
-                                    properties: [
-                                        {
-                                            startValue: 400,
-                                            endValue: 0,
-                                            property: 'translateY',
-                                        },
-                                    ],
-                                },
-                                {
-                                    start: 'self',
-                                    duration: 700,
-                                    easing: 'easeOut',
-                                    properties: [
-                                        {
-                                            startValue: 0,
-                                            endValue: 1,
-                                            property: 'opacity',
-                                        },
-                                    ],
-                                },
-                            ]}
-                        >
-                            <RandomQuote uniq={type} />
-                        </Plx>
+                        {timeline && (
+                            <Plx
+                                parallaxData={[
+                                    {
+                                        start: 'self',
+                                        duration: 150,
+                                        easing: 'easeOut',
+                                        properties: [
+                                            {
+                                                startValue: 400,
+                                                endValue: 0,
+                                                property: 'translateY',
+                                            },
+                                        ],
+                                    },
+                                    {
+                                        start: 'self',
+                                        duration: 700,
+                                        easing: 'easeOut',
+                                        properties: [
+                                            {
+                                                startValue: 0,
+                                                endValue: 1,
+                                                property: 'opacity',
+                                            },
+                                        ],
+                                    },
+                                ]}
+                            >
+                                <RandomQuote uniq={thingType} />
+                            </Plx>
+                        )}
                     </>
                 </div>
             </div>
