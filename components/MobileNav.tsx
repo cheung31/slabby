7857f079b9f09a344dbeb2c9f87bb29d9c768532
Ryan Cheung <cheung31@gmail.com>
2022-05-15 14:05:32 -0700

    Refactor MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,37 +1,57 @@
 import React from 'react'
-import Link from 'next/link'
-import BlurOverlay from '../components/BlurOverlay'
+import NextLink from 'next/link'
+import BlurOverlay from './BlurOverlay'
 import styles from '../styles/mobileNav.module.css'
+
+type NavButtonProps = {
+    isActive?: boolean
+    className?: string
+    children: React.ReactNode
+}
+const NavButton = (props: NavButtonProps) => {
+    const { isActive = false, className, children } = props
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+        if (e.target && (e.code === 'Space' || e.code === 'Enter')) {
+            const target = e.currentTarget as HTMLElement
+            ;(target.firstElementChild as HTMLElement).click()
+        }
+    }
+
+    return (
+        <BlurOverlay
+            className={`flex bg-opacity-50 dark:bg-opacity-50 hover:bg-opacity-40 dark:hover:bg-opacity-70 ${className}`}
+        >
+            <button
+                className={`flex-1 ${isActive ? styles.active : styles.tab}`}
+                onKeyDown={handleKeyDown}
+            >
+                {children}
+            </button>
+        </BlurOverlay>
+    )
+}
 
 type MobileNavProps = {
     pathname?: string | string[]
     className?: string
 }
-
 const MobileNav = ({ pathname = '', className = '' }: MobileNavProps) => {
     return (
-        <div
+        <nav
             className={`flex font-mono shadow-lg dark:text-gray-300 w-screen ${className}`}
         >
-            <BlurOverlay
-                className={`flex-1 p-3 pt-2 bg-opacity-50 dark:bg-opacity-50 hover:bg-opacity-40 dark:hover:bg-opacity-70 ${
-                    pathname === 'photo' ? styles.active : styles.tab
-                }`}
-            >
-                <Link href="/photo" passHref>
-                    <div className="cursor-pointer">photos</div>
-                </Link>
-            </BlurOverlay>
-            <BlurOverlay
-                className={`flex-1 p-3 pt-2 bg-opacity-50 dark:bg-opacity-50 hover:bg-opacity-40 dark:hover:bg-opacity-70 ${
-                    pathname === 'tune' ? styles.active : styles.tab
-                }`}
-            >
-                <Link href="/tune" passHref>
-                    <div className="cursor-pointer">tunes</div>
-                </Link>
-            </BlurOverlay>
-        </div>
+            <NavButton className="flex-1" isActive={pathname === 'photo'}>
+                <NextLink href="/photo" passHref>
+                    <div className="cursor-pointer p-3 pt-2">photos</div>
+                </NextLink>
+            </NavButton>
+            <NavButton className="flex-1" isActive={pathname === 'tune'}>
+                <NextLink href="/tune" passHref>
+                    <div className="cursor-pointer p-3 pt-2">tunes</div>
+                </NextLink>
+            </NavButton>
+        </nav>
     )
 }
 
