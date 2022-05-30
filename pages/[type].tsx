4a109a607a000aea30e5439ef2e348d68f862110
Ryan Cheung <cheung31@gmail.com>
2022-05-29 18:01:14 -0700

    No SSR

--- pages/[type].tsx
+++ pages/[type].tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import type { GetServerSideProps, NextPage } from 'next'
+import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import Plx from 'react-plx'
 import { Helmet } from 'react-helmet'
@@ -7,11 +7,8 @@ import MobileNav from '../components/MobileNav'
 import RandomQuote from '../components/RandomQuote'
 import GenericTimeline from '../components/GenericTimeline'
 import { isThingType, ThingType } from '../types/things'
-import { TimelineItem, VisibleItem } from '../types/timeline'
-import { supabase } from '../utils/supabaseClient'
-import { definitions } from '../types/supabase'
-import { utcStringToTimestampz } from '../utils'
-import { TYPE_PUBLISH_DELAY_MS, typeOptions } from '../config'
+import { TimelineItem } from '../types/timeline'
+import { typeOptions } from '../config'
 
 type IndexProps = {
     thingType?: ThingType
@@ -120,51 +117,3 @@ const Index: NextPage<IndexProps> = ({ thingType: t, timelineItems }) => {
 }
 
 export default Index
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getServerSideProps: GetServerSideProps<{
-    type?: ThingType
-    timelineItems?: TimelineItem[]
-}> = async ({ params }) => {
-    const props = {}
-    const type = params?.type
-    if (!type || !isThingType(type)) return { props }
-
-    const limit = 2
-    const rangeFrom = 0
-    const rangeTo = rangeFrom + limit - 1
-    const contentDateOffset = `${utcStringToTimestampz(
-        ((Date.now() - TYPE_PUBLISH_DELAY_MS[type]) / 1000).toString()
-    )}`
-    const { data: things, error } = await supabase
-        .from<definitions['things']>('things')
-        .select()
-        .eq('type', type)
-        .is('deleted_at', null)
-        .not('image_url', 'is', null)
-        .lte('content_date', contentDateOffset)
-        .order('content_date', { ascending: false })
-        .limit(limit)
-        .range(rangeFrom, rangeTo)
-
-    if (error) return { props }
-
-    const visibleItems: VisibleItem[] = things
-        .sort(
-            (a, b) =>
-                new Date(b.content_date ?? '').getTime() -
-                new Date(a.content_date ?? '').getTime()
-        )
-        .map((t) => ({
-            ...t,
-            visible: true,
-            queued: false,
-        }))
-
-    return {
-        props: {
-            type,
-            timelineItems: visibleItems,
-        },
-    }
-}
