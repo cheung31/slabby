8b8cc00ceaaa5a94bef5894fdceebc2b0b470880
Ryan Cheung <cheung31@gmail.com>
2022-12-20 14:59:03 -0700

    Add migration for things.posted_at

---
+++ supabase/migrations/20221220215425_add_things_posted_at.sql
@@ -0,0 +1,4 @@
+alter table "public"."things" add column "posted_at" timestamp with time zone;
+
+-- Backfill posted_at from content_date
+UPDATE "public"."things" SET "posted_at" = "content_date" WHERE "type" = 'photo';
